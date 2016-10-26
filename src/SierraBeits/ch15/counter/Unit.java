1cd78c63d08a4eafaad563f5074b594e2b7b996a
AntonWork <cubsplanet@gmail.com>
2016-10-26 16:30:17 +0300

    Add simple counter in ch15

---
+++ src/SierraBeits/ch15/counter/Unit.java
@@ -0,0 +1,27 @@
+package SierraBeits.ch15.counter;
+
+public class Unit  {
+
+    private int countAmount;
+    private  static Unit unit;
+
+    public int getCountAmount() {
+        return countAmount;
+    }
+
+    private Unit (){
+
+    }
+
+    public static Unit  getUnit(){
+        if (unit==null) unit = new Unit();
+         return unit;
+    }
+
+
+    public synchronized void increment (){
+        countAmount++;
+    }
+
+
+}
